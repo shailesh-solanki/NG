c9250640bbb50b8980b6c6a6138f3248c9b18459
shailesh-solanki <36564187+shailesh-solanki@users.noreply.github.com>
2023-01-23 13:27:58 +0530

    Add files via upload

---
+++ PROCEDURES/PR_AKS_TEST.sql
@@ -0,0 +1,17 @@
+--------------------------------------------------------
+--  DDL for Procedure PR_AKS_TEST
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE PROCEDURE "ASB_STG"."PR_AKS_TEST" 
+(
+pv_text_identifier in varchar2
+)
+is
+begin
+insert into aks_test ( text_identifier ) values ( pv_text_identifier );
+commit;
+end;
+
+/
+
